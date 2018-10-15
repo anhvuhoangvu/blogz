48e935fc315c686bef3eaa8686b3f9681090b2e5
Anh Vu <Anhvuhoangvu@gmail.com>
2018-10-15 14:51:48 -0500

    finished templates and main, fixed

--- main.py
+++ main.py
@@ -1,50 +1,95 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+# importing datetime for bonus in reverse ordering blog posts
+from datetime import datetime
+from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+# SQL database configuration: username:password@server:portnumber/databasename
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+# randomly generated secret_key
+app.secret_key = 'xfd{H\xe5<\xf9\x6a2\xa0\x9fR"\xa1\xa8'
 
+# creating blog persistent class
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1200))
+    created = db.Column(db.DateTime)
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+# created date for reverse ordering posts
+        self.created = datetime.utcnow()
 
-class Task(db.Model):
+    def is_valid(self):
+        if self.title and self.body and self.created:
+            return True
+        else:
+            return False
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
-
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
+    return redirect('/blog')
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+# route to main blog page
+@app.route('/blog')
+def blog_index():
+    blog_id = request.args.get('id')
+    blogs = Blog.query.all()
+
+    if blog_id:
+        post = Blog.query.get(blog_id)
+        blog_title = post.title
+        blog_body = post.body
+# Use Case 1: Click on a blog entry's title on the main page and go to a blog's individual entry page.
+        return render_template('entry.html', title="Blog Entry #" + blog_id, blog_title=blog_title, blog_body=blog_body)
+
+# TODO - Sort posts from newest to oldest
+    sort = request.args.get('sort')
+
+    if (sort=="newest"):
+        blogs = Blog.query.order_by(Blog.created.desc()).all()
+    elif (sort=="oldest"):
+        blogs = Blog.query.order_by(Blog.created.asc()).all()
+    else:
+        blogs = Blog.query.all()
+    return render_template('blog.html', title="Build A Blog", blogs=blogs)
+
+# handler route to new post page.
+@app.route('/post')
+def new_post():
+    return render_template('post.html', title="Add New Blog Entry")
+
+# handler route to validate post title & body fields
+@app.route('/post', methods=['POST'])
+def verify_post():
+    blog_title = request.form['title']
+    blog_body = request.form['body']
+    title_error = ''
+    body_error = ''
+
+# error validation messages, if blog/title is empty return error text.
+    if blog_title == "":
+        title_error = "Title required."
+    if blog_body == "":
+        body_error = "Content required."
+
+# add new blog post and commit it to table with new id.
+    if not title_error and not body_error:
+        new_blog = Blog(blog_title, blog_body)
+        db.session.add(new_blog)
         db.session.commit()
-
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
-
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
-
+        blog = new_blog.id
+# Use Case 2: After adding a new blog post, instead of going back to the main page, we go to that blog post's individual entry page. Redirect to specific blog id page.
+        return redirect('/blog?id={0}'.format(blog))
+    else:
+# return user to post page with errors.
+        return render_template('post.html', title="Add New Blog Entry", blog_title = blog_title, blog_body = blog_body, title_error = title_error, body_error = body_error)
 
 if __name__ == '__main__':
     app.run()
